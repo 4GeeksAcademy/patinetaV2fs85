2318053f8d85a960d089bb6bf199fef32451ccf6
kevinpg93 <keeviin.93@gmail.com>
2025-02-17 19:43:57 +0000

    Crate database

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,8 +1,12 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User
+from .models import db, User, City, Interest_point, Restaurant, Hotel, Favorites
 from flask_admin.contrib.sqla import ModelView
+
+class FavoritesView(ModelView):
+    column_list = ("favorites_user_id","favorites_hotel_id","favorites_city_id", "favorites_interest_point_id", 'favorites_restaurant_id')
+    form_columns = ("favorites_user_id","favorites_hotel_id","favorites_city_id", "favorites_interest_point_id", 'favorites_restaurant_id')
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
@@ -10,8 +14,13 @@ def setup_admin(app):
     admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
 
     
-    # Add your models here, for example this is how we add a the User model to the admin
+    # Add your models here, for example this is how we add a the User model to the admin+
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(City, db.session))
+    admin.add_view(ModelView(Restaurant, db.session))
+    admin.add_view(ModelView(Interest_point, db.session))
+    admin.add_view(ModelView(Hotel, db.session))
+    admin.add_view(FavoritesView(Favorites, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,113 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, String, Boolean
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import  relationship
+from typing import List
+
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    id = db.Column(Integer, primary_key=True)
+    email = db.Column(String(120),  nullable=False)
+    password = db.Column(String(80), nullable=False)
+    is_active = db.Column(Boolean(),  nullable=False)
+    favorites=db.relationship("Favorites", backref="user")
+    city=db.relationship("City", backref="user")
+    restaurant=db.relationship("Restaurant", backref="user")
+    hotel=db.relationship("Hotel", backref="user")
+    interest_point=db.relationship("Interest_point", backref="user")
 
-    def __repr__(self):
-        return f'<User {self.email}>'
-
+    
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+    
+class City (db.Model):
+    __tablename__ = "city"
+    id = db.Column(Integer, primary_key=True)
+    city_name = db.Column(String(30), nullable=False)
+    country_name = db.Column(String(30),nullable=False)
+    user_id= db.Column(db.Integer, db.ForeignKey("user.id"))
+    favorites =db.relationship("Favorites", backref="city")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "city_name": self.city_name,
+            "country_name": self.country_name
+            # do not serialize the password, its a security breach
+        }
+    
+class Restaurant (db.Model):
+    __tablename__ = "restaurant"
+    id =db.Column(Integer,primary_key=True)
+    restaurant_name = db.Column (String(30), nullable=False)
+    address = db.Column(String(100), nullable=False)
+    user_id= db.Column(db.Integer, db.ForeignKey("user.id"))
+    favorites =db.relationship("Favorites", backref="restaurant")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "restaurant_name": self.restaurant_name,
+            "address": self.address
+            # do not serialize the password, its a security breach
+        }
+    
+class Interest_point(db.Model):
+    __tablename__ = "interest_point"
+    id = db.Column(Integer, primary_key=True)
+    int_name = db.Column(String(100), nullable=False)
+    locality = db.Column(String(100), nullable=False)
+    point_address = db.Column(String(100), nullable=False)
+    user_id= db.Column(db.Integer, db.ForeignKey("user.id"))
+    favorites =db.relationship("Favorites", backref="interest_point")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "int_name": self.int_name,
+            "point_address": self.point_address
+            # do not serialize the password, its a security breach
+        }
+    
+class Hotel(db.Model):
+    __tablename__ = "hotel"
+    id = db.Column(Integer, primary_key=True)
+    hotel_name = db.Column(String(50), nullable=False)
+    hotel_address = db.Column(String(100), nullable=False)
+    user_id= db.Column(db.Integer, db.ForeignKey("user.id"))
+    favorites =db.relationship("Favorites", backref="hotel")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "hotel_name": self.hotel_name,
+            "hotel_address": self.hotel_address
+            # do not serialize the password, its a security breach
+        }
+    
+class Favorites (db.Model):
+    __tablename__ = "favorites"
+    id = db.Column(Integer, primary_key=True)
+    favorites_user_id= db.Column(db.Integer, db.ForeignKey("user.id"))
+    favorites_city_id= db.Column(db.Integer, db.ForeignKey("city.id"))
+    favorites_hotel_id= db.Column(db.Integer, db.ForeignKey("hotel.id"))
+    favorites_restaurant_id= db.Column(db.Integer, db.ForeignKey("restaurant.id"))
+    favorites_interest_point_id= db.Column(db.Integer, db.ForeignKey("interest_point.id"))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "hotel_id": self.favorites_hotel_id,
+            "interest_point": self.favorites_interest_point_id,
+            "restaurant_id": self.favorites_restaurant_id,
+            "city_id": self.favorites_city_id            
+            
+
             # do not serialize the password, its a security breach
         }
